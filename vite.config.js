0b6b0f9a7b0837c5fc307d4c6a12ae90763d2350
ali414a44ali <ali414a44ali@gmail.com>
2026-05-29 21:06:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["icon-192.png", "icon-512.png", "sound-notify.mp3"],
-      manifest: false,
-      workbox: { globPatterns: ["**/*.{js,css,html,mp3,png}"] },
-    }),
-  ],
+  plugins: [react()],
+  base: "./",
 });
