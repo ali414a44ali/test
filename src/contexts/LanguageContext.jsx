1ff7ef256944733f2291235d7ccbd180a7be1636
ali414a44ali <ali414a44ali@gmail.com>
2026-05-29 20:07:59 +0300

    Update LanguageContext.jsx

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -11,6 +11,10 @@ export function LanguageProvider({ children }) {
     document.documentElement.dir = lang === "ar" ? "rtl" : "ltr";
     document.documentElement.lang = lang;
   }, [lang]);
-  return <LanguageContext.Provider value={{ lang, setLang }}>{children}</LanguageContext.Provider>;
+  return (
+    <LanguageContext.Provider value={{ lang, setLang }}>
+      {children}
+    </LanguageContext.Provider>
+  );
 }
 export const useLanguage = () => useContext(LanguageContext);
